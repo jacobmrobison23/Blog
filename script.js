413c02ed667ce0f299188305a0ca6a7696fff602
Jacob <jacobmrobison472@gmail.com>
2024-07-05 19:36:03 -0400

    css and java variables for local storage

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// define variables
+const postForm = document.querySelector("form");
+const nameInput = document.querySelector("#formUsername");
+const titleInput = document.querySelector("#formTitle");
+const contentInput = document.querySelector("#formcontent");
