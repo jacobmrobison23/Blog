bd0ed5f15513fc692f167c851092620e00eeefd8
Jacob <jacobmrobison472@gmail.com>
2024-07-10 11:36:58 -0400

    fixed minor bug

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 // define variables
-const postForm = document.querySelector("form");
+const postForm = document.querySelector("#blogForm");
 const nameInput = document.querySelector("#formUsername");
 const titleInput = document.querySelector("#formTitle");
 const contentInput = document.querySelector("#formcontent");
+
+
