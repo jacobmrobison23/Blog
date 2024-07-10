ea604a8977eedde104876f91b1dda5a843736971
Jacob <jacobmrobison472@gmail.com>
2024-07-10 12:02:37 -0400

    storage code

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ const titleInput = document.querySelector("#formTitle");
 const contentInput = document.querySelector("#formcontent");
 
 
+document.getElementById(`blogForm`).addEventListener(`Submit`, function(event) {
+    event.preventDefault();
+
+    const nameInput = document.getElementById('formUsername').value;
+    const titleInput = document.getElementById('formTitle').value;
+    const contentInput = document.getElementById('formContent').value;
+
+    const data = {
+        nameInput: username,
+        titleInput: title,
+        contentInput: content
+    };
+
+    saveFormData(data);
+});
+
+function saveFormData(formData) {
+    const storedFormData = JSON.parse(localStorage.getItem('data')) || [];
+
+    storedFormData.push(formData);
+
+    localStorage.setItem('formData', JSON.stringify(storedFormData));
+}
